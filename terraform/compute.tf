4fb34d87951c9cfd93151906027f48b8872a8fb7
t.zvezdov <tzvezdov@tzvezdov.tinqin.com>
2026-01-15 23:30:27 +0200

    delete instance

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "test" {
-  ami           = "ami-08eb150f611ca277f" # Amazon Linux 2023 in eu-north-1
-  instance_type = "t3.micro"
+# resource "aws_instance" "test" {
+#   ami           = "ami-08eb150f611ca277f" # Amazon Linux 2023 in eu-north-1
+#   instance_type = "t3.micro"
 
-  tags = {
-    Name = "terraform-test-instance"
-  }
-}
+#   tags = {
+#     Name = "terraform-test-instance"
+#   }
+# }
